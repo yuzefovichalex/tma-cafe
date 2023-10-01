10ec5550f06b38e26cc330cc5c40959c8fa94051
Yuzefovich Alexander <yuzefovich.alexander@gmail.com>
2023-10-01 23:13:23 +0200

    Prepare frontend for deploy on Netlify

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    'http://127.0.0.1:5500'
+    'http://localhost:8888'
 ]
 
 app.add_middleware(
--- frontend/js/requests/requests.js
+++ frontend/js/requests/requests.js
@@ -1,8 +1,6 @@
-const baseApiUrl = 'http://127.0.0.1:8000/api'
-
 export function get(endpoint, onSuccess) {
     $.ajax({
-        url: baseApiUrl + endpoint,
+        url: '/api' + endpoint,
         dataType: "json",
         success: result => onSuccess(result)
     });
