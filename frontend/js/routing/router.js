d24778461931e7f33b7bfc2ec5632b5c3c40d880
Yuzefovich Alexander <yuzefovich.alexander@gmail.com>
2023-10-06 03:38:58 +0200

    Implement navigation transitions

--- frontend/js/routing/router.js
+++ frontend/js/routing/router.js
@@ -11,16 +11,19 @@ const routes = [
     new CartPage()
 ]
 
+var pendingAnimations = false
+var animationRunning = false
+
 export const navigateTo = (dest, params) => {
     var url = '?dest=' + dest;
     if (params != null) {
         url += '&params=' + encodeURIComponent(params);
     }
     window.history.pushState({}, '', url);
-    handleLocation();
+    handleLocation(false);
 };
 
-export const handleLocation = () => {
+export const handleLocation = (reverse) => {
     const search = window.location.search;
     if (search === "") {
         navigateTo('root')
@@ -32,7 +35,20 @@ export const handleLocation = () => {
             var loadParams = decodeURIComponent(encodedLoadParams);
         }
         const route = routes.find((route) => dest === route.dest);
-        $('#content').load(route.contentPath, () => route.load(loadParams));
+
+        if (animationRunning) {
+            pendingAnimations = true;
+            return;
+        }
+
+        if ($('#page-current').contents().length > 0) {
+            $('#page-next').load(route.contentPath, () => {
+                animatePageChange(reverse);
+                route.load(loadParams);
+            });
+        } else {
+            $('#page-current').load(route.contentPath, () => route.load(loadParams));
+        }
 
         if (route.dest !== 'root') {
             TelegramSDK.showBackButton(() => history.back());
@@ -42,6 +58,53 @@ export const handleLocation = () => {
     }
 };
 
-window.onpopstate = handleLocation;
+function animatePageChange(reverse) {
+    animationRunning = true;
+            
+    const currentPageZIndex = reverse ? '2' : '1';
+    const currentPageLeftTo = reverse ? '100vw' : '-25vw';
+    const nextPageZIndex = reverse ? '1' : '2';
+    const nextPageLeftFrom = reverse ? '-25vw' : '100vw';
+
+    $('#page-current')
+        .css({
+            transform: '',
+            'z-index': currentPageZIndex
+        })
+        .transition({ x: currentPageLeftTo }, 325);
+
+    $('#page-next')
+        .css({
+            display: '',
+            transform: `translate(${nextPageLeftFrom})`,
+            'z-index': nextPageZIndex
+        })
+        .transition({ x: '0px' }, 325, () => { 
+            animationRunning = false;
+            restorePagesInitialState();
+            if (pendingAnimations) {
+                pendingAnimations = false;
+                handleLocation(reverse);
+            }
+        });
+}
+
+function restorePagesInitialState() {
+    $('#page-current')
+        .css({
+            transform: '',
+            'z-index': '2'
+        })
+        .empty();
+
+    $('#page-next')
+        .css({
+            display: 'none',
+            'z-index': '1'
+        })
+        .contents().appendTo('#page-current');
+}
+
+window.onpopstate = () => handleLocation(true);
 
 handleLocation();
