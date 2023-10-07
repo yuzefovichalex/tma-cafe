641779399db8721497493ac161260cb56242d660
Yuzefovich Alexander <yuzefovich.alexander@gmail.com>
2023-10-07 18:48:35 +0200

    Improve cart items update operations

--- frontend/js/pages/cart.js
+++ frontend/js/pages/cart.js
@@ -20,9 +20,47 @@ export class CartPage extends Route {
     }
 
     #fillCartItems(cartItems) {
+        this.#updateMainButton(cartItems);
+
+        const cartItemsContainer = $('#cart-items');
+        const cartItemsIds = cartItems.map((cartItem) => cartItem.getId());
+
+        // Remove elements not presented in the cartItems anymore
+        cartItemsContainer.children().each((index, element) => {
+            const cartItemElement = $(element);
+            if (!cartItemsIds.includes(cartItemElement.attr('id'))) {
+                cartItemElement.remove();
+            }
+        })
+
+        const cartItemTemplateHtml = $('#cart-item-template').html();
+        cartItems.forEach(cartItem => {
+            const cartItemElement = cartItemsContainer.find(`#${cartItem.getId()}`);
+            if (cartItemElement.length > 0) {
+                // We found the existing item, just update the needed params
+                cartItemElement.find('#cart-item-quantity').text(cartItem.quantity);
+            } else {
+                // This is the newely added item, create new element for it
+                const filledCartItemTemplate = $(cartItemTemplateHtml);
+                filledCartItemTemplate.attr('id', `${cartItem.getId()}`)
+                loadImage(filledCartItemTemplate.find('#cart-item-image'), cartItem.cafeItem.image);
+                filledCartItemTemplate.find('#cart-item-name').text(cartItem.cafeItem.name);
+                filledCartItemTemplate.find('#cart-item-quantity').text(cartItem.quantity);
+                filledCartItemTemplate.find('#cart-item-quantity-increment').on('click', () => {
+                    Cart.addItem(cartItem.cafeItem, 1);
+                });
+                filledCartItemTemplate.find('#cart-item-quantity-decrement').on('click', () => {
+                    Cart.removeItem(cartItem.cafeItem, 1);
+                });
+                cartItemsContainer.append(filledCartItemTemplate);
+            }
+        });
+    }
+
+    #updateMainButton(cartItems) {
         if (cartItems.length > 0) {
             TelegramSDK.showMainButton(
-                'Checkout',
+                'CHECKOUT',
                 () => post('/order', JSON.stringify(cartItems), (result) => {
                     TelegramSDK.openInvoice(result.invoiceUrl);
                 })
@@ -30,23 +68,5 @@ export class CartPage extends Route {
         } else {
             TelegramSDK.hideMainButton();
         }
-
-        const cartItemContainer = $('#cart-items');
-        cartItemContainer.empty();
-
-        const cartItemTemplateHtml = $('#cart-item-template').html();
-        cartItems.forEach(cartItem => {
-            const filledCartItemTemplate = $(cartItemTemplateHtml);
-            loadImage(filledCartItemTemplate.find('#cart-item-image'), cartItem.cafeItem.image);
-            filledCartItemTemplate.find('#cart-item-name').text(cartItem.cafeItem.name);
-            filledCartItemTemplate.find('#cart-item-quantity').text(cartItem.quantity);
-            filledCartItemTemplate.find('#cart-item-quantity-increment').on('click', () => {
-                Cart.addItem(cartItem.cafeItem, 1);
-            });
-            filledCartItemTemplate.find('#cart-item-quantity-decrement').on('click', () => {
-                Cart.removeItem(cartItem.cafeItem, 1);
-            });
-            cartItemContainer.append(filledCartItemTemplate);
-        });
     }
 }
